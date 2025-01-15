710a43c06102ee331956c6fd1a43c336a33fb472
Sam Warren <samwarren4@gmail.com>
2025-01-15 01:34:14 -0800

    Add Vercel analytics and speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import HeroSection from "./components/home/HeroSection";
 import Layout from "./components/layout/Layout";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   return (
-    <Layout>
-      <HeroSection />
-    </Layout>
+    <div>
+      <Analytics />
+      <SpeedInsights />
+      <Layout>
+        <HeroSection />
+      </Layout>
+    </div>
   );
 }
 
